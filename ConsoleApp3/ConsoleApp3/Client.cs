87cb31f3eccab7b60d36f8387c19d91ac333a967
NAEL VALIN <nael.valin@epita.fr>
2021-02-22 15:37:10 +0000

    Parking OOP

---
+++ ConsoleApp3/ConsoleApp3/Client.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp3
+{
+    public class Client
+    {
+        public string name { get; set; }
+        public int hour { get; set; }
+
+        public Client(string _name, int _hour)
+        {
+            name = _name;
+            hour = _hour;
+        }
+        public int CalculateCharges()
+        {
+            int price;
+            if (hour == 24)
+                price = 10;
+            else if (hour > 3)
+                price = 2 + (hour - 3) / 2;
+            else
+                price = 2;
+            return price;
+        }
+        public override string ToString()
+        {
+            return $"{name} payed {CalculateCharges()}.00 $ because he stayed {hour} hours on the parking";
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client1 = new Client("M. Couillon",15);
+            Client client2 = new Client("Misses Debile", 24);
+            Console.WriteLine(client1);
+            Console.WriteLine(client2);
+        }
+    }
+}
